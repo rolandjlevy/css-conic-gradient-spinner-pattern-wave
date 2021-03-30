3967b2935e5ad850814b8c920c6b559f4ef0d3dd
Roland Levy <contact@rolandlevy.co.uk>
2021-03-30 20:05:47 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const $ = (el) => document.querySelector(el);
+
+const maxSpeed = $('.slider').max;
+const minSpeed = $('.slider').min;
+
+const amount = 64;
+let counter = 0;
+
+while (counter++ < amount) {
+  const li = document.createElement('li');
+  li.setAttribute('data-id', counter);
+  li.style.setProperty('--delay', `${counter/12}s`);
+  const dir = counter % 2 ? 'normal' : 'normal';
+  li.classList = `conic-spinner ${dir}`;
+  $('.spinners').appendChild(li);
+}
+
+$('.slider').addEventListener('input', (e) => {
+  const speed = Number(maxSpeed) + Number(minSpeed) - Number(e.target.value);
+  document.documentElement.style.setProperty('--speed', speed + "s");
+});
