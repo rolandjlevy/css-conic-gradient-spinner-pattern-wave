387d5b6d891bc8747917abfad4a98e394f15174a
Roland Levy <contact@rolandlevy.co.uk>
2021-03-30 20:47:55 +0000

    Improved wave effect

--- script.js
+++ script.js
@@ -2,15 +2,14 @@ const $ = (el) => document.querySelector(el);
 
 const maxSpeed = $('.slider').max;
 const minSpeed = $('.slider').min;
-
-const amount = 64;
+const amount = 100;
 let counter = 0;
 
 while (counter++ < amount) {
   const li = document.createElement('li');
   li.setAttribute('data-id', counter);
-  li.style.setProperty('--delay', `${counter/12}s`);
-  const dir = counter % 2 ? 'normal' : 'normal';
+  li.style.setProperty('--delay', `-${counter/12}s`);
+  const dir = counter % 2 ? 'normal' : 'reverse';
   li.classList = `conic-spinner ${dir}`;
   $('.spinners').appendChild(li);
 }
